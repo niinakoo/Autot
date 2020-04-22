f4739d94afad4ad9ccfcfdaaed9204818a436522
Niina <niina.kuusisto@myy.haaga-helia.fi>
2020-04-22 18:58:59 +0300

    Haku lisätty

--- src/control/Autot.java
+++ src/control/Autot.java
@@ -12,37 +12,41 @@ import org.json.JSONObject;
 import model.Auto;
 import model.dao.Dao;
 
-@WebServlet("/autot")
+
+@WebServlet("/autot/*")
 public class Autot extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    
+   
     public Autot() {
         super();
         System.out.println("Autot.Autot()");
     }
-
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Autot.doGet()");
+		String pathInfo = request.getPathInfo();			//haetaan kutsun polkutiedot, esim. /audi			
+		System.out.println("polku: "+pathInfo);	
+		String hakusana = pathInfo.replace("/", "");
 		Dao dao = new Dao();
-		ArrayList<Auto> autot = dao.listaaKaikki();
+		ArrayList<Auto> autot = dao.listaaKaikki(hakusana);
 		System.out.println(autot);
-		String strJSON = new JSONObject().put("autot", autot).toString();
+		String strJSON = new JSONObject().put("autot", autot).toString();	
 		response.setContentType("application/json");
 		PrintWriter out = response.getWriter();
-		out.println(strJSON);
+		out.println(strJSON);		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Autot.doPost()");
+		System.out.println("Autot.doPost()");		
 	}
 
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Autot.doPut()");
+		System.out.println("Autot.doPut()");		
 	}
-
+	
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Autot.doDelete()");
+		System.out.println("Autot.doDelete()");		
 	}
 
 }
